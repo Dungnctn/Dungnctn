f9cc35699cdb047b115f5fa9ddc77a87974d99ad
Chi Dung <Dungnc0402@gmail.com>
2022-01-20 21:26:34 +0700

    update

--- data/data.js
+++ data/data.js
@@ -3,7 +3,7 @@ const data = [
         "createdAt": "2022-01-10T07:26:52.549Z",
         "title": "Annie Stroman",
         "price": "80000",
-        "img": "../fontend/images/product_1.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f21tss030_-_black_1__1.jpg",
         "desc": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
         "id": "1"
       },
@@ -11,7 +11,7 @@ const data = [
         "createdAt": "2022-01-10T20:33:27.477Z",
         "title": "Yolanda Roob",
         "price": "80000",
-        "img": "../fontend/images/product_1.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f21jac007-tigerseye-2_1.jpg",
         "desc": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
         "id": "2"
       },
@@ -19,7 +19,7 @@ const data = [
         "createdAt": "2022-01-11T03:36:13.571Z",
         "title": "Rufus Konopelski",
         "price": "80000",
-        "img": "../fontend/images/product_2.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f21jac008-monkrobe-1_1.jpg",
         "desc": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
         "id": "3"
       },
@@ -27,7 +27,7 @@ const data = [
         "createdAt": "2022-01-10T04:45:32.422Z",
         "title": "Ray Boyle",
         "price": "80000",
-        "img": "../fontend/images/product_3.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f21jac011-sliver_grey-1_1.jpg",
         "desc": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
         "id": "4"
       },
@@ -35,7 +35,7 @@ const data = [
         "createdAt": "2022-01-10T10:56:22.073Z",
         "title": "Jacqueline Hills",
         "price": "80000",
-        "img": "../fontend/images/product_4.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10s21drew024-1_1.jpg",
         "desc": "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
         "id": "5"
       },
@@ -43,7 +43,7 @@ const data = [
         "createdAt": "2022-01-11T00:27:12.664Z",
         "title": "Lee Abbott",
         "price": "80000",
-        "img": "../fontend/images/product_5.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f20dpaw025.0_1.jpg",
         "desc": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
         "id": "6"
       },
@@ -51,7 +51,7 @@ const data = [
         "createdAt": "2022-01-10T14:41:53.412Z",
         "title": "Muriel Okuneva",
         "price": "80000",
-        "img": "../fontend/images/product_6.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10f20shlw008-be.0_1.jpg",
         "desc": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
         "id": "7"
       },
@@ -59,7 +59,7 @@ const data = [
         "createdAt": "2022-01-10T16:35:33.134Z",
         "title": "Tara Gutmann",
         "price": "80000",
-        "img": "../fontend/images/product_7.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/l/i/liqui0913_2.jpg",
         "desc": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
         "id": "8"
       },
@@ -67,7 +67,7 @@ const data = [
         "createdAt": "2022-01-10T21:17:56.264Z",
         "title": "Rudy Trantow",
         "price": "80000",
-        "img": "../fontend/images/product_8.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10s21pol024-black.1_1.jpg",
         "desc": "The Football Is Good For Training And Recreational Purposes",
         "id": "9"
       },
@@ -75,7 +75,7 @@ const data = [
         "createdAt": "2022-01-10T23:35:59.996Z",
         "title": "Wayne Lang V",
         "price": "80000",
-        "img": "../fontend/images/product_9.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/5/_/5.8_1.jpg",
         "desc": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
         "id": "10"
       },
@@ -83,7 +83,7 @@ const data = [
         "createdAt": "2022-01-10T14:22:10.945Z",
         "title": "Esther Morar",
         "price": "80000",
-        "img": "../fontend/images/product_10.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/1/0/10.5_1.jpg",
         "desc": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
         "id": "11"
       },
@@ -91,49 +91,9 @@ const data = [
         "createdAt": "2022-01-10T11:38:45.275Z",
         "title": "Sonya Prohaska Sr.",
         "price": "80000",
-        "img": "../fontend/images/product_1.png",
+        "img": "https://routine.vn/media/catalog/product/cache/a2fcefb561dcc03dd69ce08e688653ae/_/d/_dsc0899_6.jpg",
         "desc": "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
         "id": "12"
-      },
-      {
-        "createdAt": "2022-01-10T14:45:44.448Z",
-        "title": "Levi Turcotte",
-        "price": "80000",
-        "img": "../fontend/images/product_2.png",
-        "desc": "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
-        "id": "13"
-      },
-      {
-        "createdAt": "2022-01-10T06:52:23.902Z",
-        "title": "Eddie Crona",
-        "price": "80000",
-        "img": "../fontend/images/product_3.png",
-        "desc": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
-        "id": "14"
-      },
-      {
-        "createdAt": "2022-01-10T15:09:36.220Z",
-        "title": "Heather Labadie",
-        "price": "80000",
-        "img": "../fontend/images/product_4.png",
-        "desc": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
-        "id": "15"
-      },
-      {
-        "createdAt": "2022-01-11T03:02:51.996Z",
-        "title": "Daryl Hettinger I",
-        "price": "80000",
-        "img": "../fontend/images/product_6.png",
-        "desc": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
-        "id": "16"
-      },
-      {
-        "createdAt": "2022-01-10T20:16:49.772Z",
-        "title": "Heather Miller",
-        "price": "80000",
-        "img": "../fontend/images/product_4.png",
-        "desc": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
-        "id": "17"
       }
 ];
 
